5a20540bfedc103d36333b537b9615f078795b84
deekshaV17 <deekshav017@gmail.com>
2019-05-16 20:40:50 +0530

    feat: add loading styles

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Loading = () => <div>Loading</div>;
+const StyledLoading = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Loading = () => <StyledLoading>Loading...</StyledLoading>;
 
 export default Loading;
