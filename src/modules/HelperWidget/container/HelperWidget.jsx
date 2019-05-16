e0fb000971ff9d04a118e2edbd93a2ecf03c70ce
deekshaV17 <deekshav017@gmail.com>
2019-05-16 20:36:18 +0530

    feat: add redux to HelperWidget

--- src/modules/HelperWidget/container/HelperWidget.jsx
+++ src/modules/HelperWidget/container/HelperWidget.jsx
@@ -1,24 +1,51 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import validator from 'email-validator';
 import Tabs from '../components/Tabs';
 import LiveChat from '../components/LiveChat';
 import FAQ from '../components/FAQ';
 import Mail from '../components/Mail';
 import StyledWidget from './Widget.styled';
+import { changeActiveTab, addChatMessage, sendMailRequest, showAlertRequest } from '../store/actions';
 
-const HelperWidgetContainer = () => (
-  <StyledWidget>
-    <Tabs>
-      <div label="Live Chat">
-        <LiveChat />
-      </div>
-      <div label="FAQ">
-        <FAQ />
-      </div>
-      <div label="Mail Us">
-        <Mail />
-      </div>
-    </Tabs>
-  </StyledWidget>
-);
+type Props = {
+  active: number,
+  chat: Array<Object>,
+  alert: Object,
+  dispatch: (Object) => {},
+};
 
-export default HelperWidgetContainer;
+const HelperWidgetContainer = ({ dispatch, active, chat, alert }: Props) => {
+  const sendMail = (mail) => {
+    if (!validator.validate(mail.subject)) {
+      dispatch(showAlertRequest('Invalid subject', false));
+      return false;
+    }
+    dispatch(sendMailRequest(mail));
+    return true;
+  };
+
+  return (
+    <StyledWidget>
+      <Tabs active={active} changeActiveTab={(index) => dispatch(changeActiveTab(index))}>
+        <div label="Live Chat">
+          <LiveChat chat={chat} addChatMessage={(message) => dispatch(addChatMessage(message))} />
+        </div>
+        <div label="FAQ">
+          <FAQ changeActiveTab={(index) => dispatch(changeActiveTab(index))} />
+        </div>
+        <div label="Mail Us">
+          <Mail sendMail={sendMail} alert={alert} />
+        </div>
+      </Tabs>
+    </StyledWidget>
+  );
+};
+
+const mapStateToProps = ({ tabs }) => ({
+  active: tabs.active,
+  chat: tabs.chat,
+  alert: tabs.alert,
+});
+
+export default connect(mapStateToProps)(HelperWidgetContainer);
